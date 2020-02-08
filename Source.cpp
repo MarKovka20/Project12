8cf3fdbc8b45071831797ee1c79e5caf573ddd7c
MarKovka20 <masha2000_1@mail.ru>
2020-02-08 11:42:17 +0300

    added 3+3 and function f

--- Source.cpp
+++ Source.cpp
@@ -1,7 +1,15 @@
 #include <iostream>
+
+void f()
+{
+	std::cout << "Hello, World!" << std::endl;
+}
+
 int main()
 {
-	std::cout << "Hello, World!";
-        std::cout << 1 + 1 << std::endl;
+    std::cout << 1 + 1 << std::endl;
+	std::cout << 2 + 2 << std::endl;
+	std::cout << 3 + 3 << std::endl;
+
 	return 0;
 }
